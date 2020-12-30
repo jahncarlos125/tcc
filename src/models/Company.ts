01bd5641f01cbe60e14cd4070d783eedec8edd84
Jahn Carlos <jahn.santana@vortigo.com.br>
2020-12-30 20:38:18 -0300

    Link model with database

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -1,32 +1,24 @@
-import { v4 as uuidv4 } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('companies')
 class Company {
+  @PrimaryGeneratedColumn('uuid')
   company_id: string;
 
+  @Column()
   company_name: string;
 
+  @Column()
   company_document: string;
 
+  @Column()
   company_email: string;
 
+  @Column()
   company_password: string;
 
+  @Column()
   company_token: string;
-
-  constructor({
-    company_name,
-    company_document,
-    company_email,
-    company_password,
-    company_token,
-  }: Omit<Company, 'company_id'>) {
-    this.company_id = uuidv4();
-    this.company_name = company_name;
-    this.company_document = company_document;
-    this.company_email = company_email;
-    this.company_password = company_password;
-    this.company_token = company_token;
-  }
 }
 
 export default Company;
